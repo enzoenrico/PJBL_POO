bd3a840d19a0130d3c5b6f8a68a593e3bfbdcf6c
enzoenrico <e.enrico2005@gmail.com>
2024-05-10 19:38:45 -0300

    first commit

---
+++ db/DB.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB {
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}

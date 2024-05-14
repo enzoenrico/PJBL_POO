988ff70aed3a0f54b9b83399a7092ac8cd436206
Pedro Fauth <fauthpedroa@gmail.com>
2024-05-14 14:43:38 -0300

    adiciona conexxo com o banco
    arquivo .jar do JDBC
    correcoes no arquivo DB.java
    mock -> inserts de usuário para teste

--- db/DB.java
+++ db/DB.java
@@ -1,15 +1,34 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DB {
-    public static Connection getConnection() {
-        Connection con = null;
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/ez_rent";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "";
+
+    public static void main(String[] args) {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ez_rent", "root", "");
-        } catch (Exception e) {
+            Connection connection = getConnection();
+            if (connection != null) {
+                System.out.println("Conexão estabelecida com sucesso!");
+                connection.close(); // Fechando a conexão após uso
+            } else {
+                System.out.println("Falha ao estabelecer a conexão.");
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        return con;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver JDBC não encontrado.");
+            e.printStackTrace();
+            return null;
+        }
     }
 }
